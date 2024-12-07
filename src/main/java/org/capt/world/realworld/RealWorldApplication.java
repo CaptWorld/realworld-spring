ec8c3083f85bcb28e26e641c64e0a851585a3b20
CaptWorld <vardhanlokesh3@gmail.com>
2024-12-07 14:34:39 +0530

    spring init

---
+++ src/main/java/org/capt/world/realworld/RealWorldApplication.java
@@ -0,0 +1,13 @@
+package org.capt.world.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/capt/world/realworld/RealWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capt.world.realworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
